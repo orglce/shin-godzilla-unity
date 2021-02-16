c0eb654412f8451d4e5a1c222ec5a5a2670f0be3
Luka Dragar <D20124481@mytudublin.ie>
2021-02-16 23:14:30 +0000

    Added OffsetPursuit

---
+++ Assets/Behaviours/OffsePursue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsePursue : SteeringBehaviour
+{
+public Boid leader;
+public Vector3 targetPos;
+public Vector3 worldTarget;
+Vector3 offset;
+
+Arrive arrive;
+
+public void OnDrawGizmos()
+{
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(worldTarget, 1);
+}
+
+public override Vector3 Calculate()
+{
+        worldTarget = leader.transform.TransformPoint(offset);
+        // worldTarget = leader.transform.position + offset;
+        // offset = leader.transform.rotation * offset;
+        float distance = Vector3.Distance(transform.position, worldTarget);
+        float time = distance / boid.maxSpeed;
+
+        Debug.Log(leader.transform.rotation);
+
+        targetPos = worldTarget + (leader.velocity * time);
+
+        arrive.target = targetPos;
+        return arrive.Calculate();
+}
+
+void Start()
+{
+        offset = transform.position - leader.transform.position;
+
+        Debug.Log(offset);
+        Debug.Log(transform.TransformPoint(offset));
+
+        arrive = gameObject.AddComponent(typeof(Arrive)) as Arrive;
+        arrive.enabled = false;
+}
+}
--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -21,121 +21,21 @@ public float damping = 0.01f;
 [Range(0.0f, 1.0f)]
 public float banking = 0.1f;
 
-//
-// public Transform targetTransform;
-// public Vector3 targetVector;
-//
-// public bool playerSteeringEnabled = false;
-// public float steeringForce = 100;
-//
-// // SEEK
-// public bool seekEnabled = false;
-//
-// // FLEE
-// public bool fleeEnabled = false;
-//
-// // ARRIVE
-// public bool arriveEnabled = false;
-// public float arriveDistance = 10;
-//
-// // PATH FOLLOW
-// public bool pathFollowEnabled = false;
-// public Path path;
-//
-// // PURSUE
-// public bool pursueEnabled = false;
-// public BigBoid pursueTargetBigBoid;
-//
-// // EVADE
-// public bool evadeEnabled = false;
-// public BigBoid evadeTargetBigBoid;
-//
-
-
-public Vector3 Seek(Vector3 target)
+public void OnDrawGizmos()
 {
-        Vector3 toTarget = target - transform.position;
-        Vector3 desiredVelocity = Vector3.Normalize(toTarget) * maxSpeed;
-        Vector3 force = desiredVelocity - velocity;
-        return force;
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + acceleration);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + force * 10);
 }
-
-// public Vector3 Flee(Vector3 target)
-// {
-//         Vector3 toTarget = transform.position - target;
-//         Vector3 desiredVelocity = Vector3.Normalize(toTarget) * maxSpeed;
-//         Vector3 force = desiredVelocity - velocity;
-//         return force;
-// }
-
-public Vector3 Arrive(Vector3 target, float arriveDistance = 3.0f)
-{
-        Vector3 toTarget = target - transform.position;
-        float distance = toTarget.magnitude;
-
-        if (distance == 0)
-                return Vector3.zero;
-
-        float decelerationTweaker = 0.3f;
-        float arriveSpeed = maxSpeed;
-
-        if (distance < arriveDistance)
-                arriveSpeed = (distance / arriveDistance * decelerationTweaker) * maxSpeed;
-
-        Vector3 desiredVelocity = Vector3.Normalize(toTarget) * arriveSpeed;
-        Vector3 force = desiredVelocity - velocity;
-        return force;
-}
-
-// public Vector3 Evade(BigBoid evadeTarget)
-// {
-//         float dist = Vector3.Distance(evadeTarget.transform.position, transform.position);
-//         float time = dist / maxSpeed;
-//         Vector3 evadeTargetPos = evadeTarget.transform.position + evadeTarget.velocity * time;
-//
-//         return Flee(evadeTargetPos);
-// }
-//
-// public Vector3 PlayerSteering()
-// {
-//         Vector3 force = Vector3.zero;
-//
-//         force += Input.GetAxis("Vertical") * transform.forward * steeringForce;
-//
-//         Vector3 projectedRight = transform.right;
-//         projectedRight.y = 0;
-//         projectedRight.Normalize();
-//
-//         force += Input.GetAxis("Horizontal") * projectedRight * steeringForce * 0.2f;
-//
-//         return force;
-// }
 
 public Vector3 CalculateForce()
 {
         Vector3 force = Vector3.zero;
-
-        // if (targetTransform != null)
-        // {
-        //         targetVector = targetTransform.position;
-        // }
-        // if (seekEnabled)
-        //         f += Seek(targetVector);
-        // if (fleeEnabled)
-        //         f += Flee(targetVector);
-        // if (arriveEnabled)
-        //         f += Arrive(targetVector);
-        // if (pathFollowEnabled)
-        //         f += PathFollow();
-        // if (pursueEnabled)
-        //         f += Pursue(pursueTargetBigBoid);
-        // if (evadeEnabled)
-        //         f += Evade(evadeTargetBigBoid);
-        //
-        // if (playerSteeringEnabled)
-        // {
-        //         f += PlayerSteering();
-        // }
 
         foreach (SteeringBehaviour b in behaviours)
         {
@@ -151,7 +51,6 @@ public Vector3 CalculateForce()
                         }
                 }
         }
-
         return force;
 }
 
